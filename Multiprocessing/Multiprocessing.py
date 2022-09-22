5d33e5b3470dc0f390ed3e11d320702945dde650
Dr-Titus100 <ndedrtitusn100@gmail.com>
2022-09-22 10:59:46 -0600

    Little update added to multiprocessing scripts and notebooks. Updated Saved models as well

--- Multiprocessing/Multiprocessing.py
+++ Multiprocessing/Multiprocessing.py
@@ -14,15 +14,16 @@ from colossus.cosmology import cosmology
 ##########################################################################
 #Loading test data
 ##########################################################################
-X_test_actual = pd.read_csv("Data/X_test_actual.csv")
+X_test_actual = pd.read_csv("Data/X_test_actual_new.csv")
 
 
 ##########################################################################
 #Function to compute DK14 density
 ##########################################################################
-#----------Setting Cosmology. This is a global set up
+#----------------------------Setting Cosmology. This is a global set up
 params = {'flat': True, 'H0': 70, 'Om0': 0.286, 'Ob0': 0.046, 'sigma8': 0.82, 'ns': 0.96}
 cosmo = cosmology.setCosmology('my_cosmo', params)
+
 
 def diemer_14(split_chunk):
     '''
@@ -37,7 +38,7 @@ def diemer_14(split_chunk):
         densities into a .npy file.
     '''
     DK14_Density = []
-    # #----------Setting Cosmology.
+    # #-------------Setting Cosmology.
     # params = {'flat': True, 'H0': 70, 'Om0': 0.286, 'Ob0': 0.046, 'sigma8': 0.82, 'ns': 0.96}
     # cosmo = cosmology.setCosmology('my_cosmo', params)
     for i in range(len(split_chunk)):
@@ -50,49 +51,50 @@ def diemer_14(split_chunk):
         Density: The actual density of the halo
         '''
         Mvir, Rvir, rs, z_orig, r_mid, Density = split_chunk[i]
-        #----------------------------DK14 density
-        p_dk14_51 = profile_dk14.getDK14ProfileWithOuterTerms(M = Mvir,
-                                                              c = Rvir/rs,
+        p_dk14_51 = profile_dk14.getDK14ProfileWithOuterTerms(M = Mvir, 
+                                                              c = Rvir/rs, 
                                                               z = z_orig, mdef = 'vir', 
                                                               outer_term_names = ['pl'])
         rho_dk14_5 = p_dk14_51.density(r_mid*1000)*1e9
         squared_diff = (Density-np.log(rho_dk14_5))**2
         DK14_Density.append(squared_diff)
-    #----------------------------Save output to .npy files
+    #-------------Saving results to a .npy file
     filename = f"{datetime.today().isoformat()}.npy"
     f = open(f"DK14_mse/{filename}", "wb")
     np.save(f, DK14_Density)
     f.close()
 
-
-
+    
 ##########################################################################
 #Parallelising code
 ##########################################################################
+#-------------Delete all files ending with .npy in this directory
 mydir = "DK14_mse/"
-filelist = [f for f in os.listdir(mydir) if f.endswith(".npy") ]
-#----------------------------delete all files ending with .npy in this directory
+filelist = [f for f in os.listdir(mydir) if f.endswith(".npy")]
+
 for f in filelist:
     os.remove(os.path.join(mydir, f))
-#----------------------------number of processors to use. We use all available processors.
-processors = multiprocessing.cpu_count()
-# dk14data = X_test_actual[["Mvir_Msun.h", "Rvir_Mpc.h", "rs", "z_orig", "r_mid_phys", "Density"]].iloc[0:1000, :].values#.reshape(1,-1)
+
+#-------------Number of processes. We use all available processes.
+# processors = multiprocessing.cpu_count()
+processors = 96
+
+#-------------Splitting data. Number of chunks equals the number of processes available
+# dk14data = X_test_actual[["Mvir_Msun.h", "Rvir_Mpc.h", "rs", "z_orig", "r_mid_phys", "Density"]].iloc[0:2000, :].values#.reshape(1,-1)
 dk14data = X_test_actual[["Mvir_Msun.h", "Rvir_Mpc.h", "rs", "z_orig", "r_mid_phys", "Density"]].values#.reshape(1,-1)
 split_data = np.array_split(dk14data, processors)
 
+#-------------Parallelising code using multiprocessing
 if __name__ == '__main__':
     '''
     Each chunk of data is set to run on a different processors. 
     Each processor stores the results in a separate .npy file. 
     The files are named using time stamps.
     '''
-
     with Pool(processes=processors) as p:
     # with Pool() as p: #this line equally works as the line above
         p.map(func = diemer_14, iterable = split_data)
 
-
-
-
-
-
+        
+    
+    
